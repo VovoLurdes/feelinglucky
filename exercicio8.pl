5678e07999841357d53ede135f16afc65c2af60f
VovoLurdes <flaviog.info@gmail.com>
2015-02-11 16:02:29 -0200

    Critic exercici8 esta ok

--- exercicio8.pl
+++ exercicio8.pl
@@ -1,11 +1,21 @@
 #!/usr/bin/perl
 
-local $/ = "\n";
-
 use strict;
 use warnings FATAL => 'all';
 use diagnostics;
+use Carp;
+use English qw( -no_match_vars);
 
-my $arquivo = my $ARGV->[0];
-open(my $fh, '<', $arquivo) or die "Não foi possível abrir o arquivo '$arquivo' $!";
-while (my $solo = <$fh>){ print $solo; }
+local $INPUT_RECORD_SEPARATOR = "\n";
+
+
+eval {
+my $arquivo = $ARGV[0];
+open my $fh, '<', $arquivo or croak "Não foi possível abrir o arquivo '$arquivo' $OS_ERROR";
+
+while (my $solo = <$fh>){ print $solo or croak 'bla'; }
+
+close $fh or croak "Erro ao fechar: $CHILD_ERROR, $OS_ERROR";
+};
+
+carp "Erro: $EVAL_ERROR" if $EVAL_ERROR;
