b127f31c1a6a5dccd13822525ff8b02306643f33
VovoLurdes <flaviog.info@gmail.com>
2015-02-11 16:03:05 -0200

    Critic exercici9 esta ok

--- exercicio9.pl
+++ exercicio9.pl
@@ -1,18 +1,24 @@
 #!/usr/bin/perl
 
-local $/;
-
 use strict;
 use warnings FATAL => 'all';
 use diagnostics;
+use Carp;
+use English qw( -no_match_vars);
 
+local $INPUT_RECORD_SEPARATOR;
+
+eval {
 my $arquivo = $ARGV[0];
 
-open(my $fh, '<', $arquivo) or die "Não foi possível abrir o arquivo '$arquivo' $!";
+open my $fh, '<', $arquivo or croak "Não foi possível abrir o arquivo '$arquivo' $OS_ERROR";
 
 my $solo = <$fh>;
 
-print $solo;
+print $solo or croak 'bla';
 
-close $fh;
+close $fh or croak "Erro ao fechar: $CHILD_ERROR, $OS_ERROR";
+};
+
+carp "Erro: $EVAL_ERROR" if $EVAL_ERROR;
 
