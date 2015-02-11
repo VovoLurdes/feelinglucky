9103a203708dcb7edc15abdb578af5e98f27fcde
VovoLurdes <flaviog.info@gmail.com>
2015-02-11 16:05:06 -0200

    Corrigido alguns critics do exercicio 7

--- exercicio7.pl
+++ exercicio7.pl
@@ -1,16 +1,19 @@
 #!/usr/bin/perl
 
-local $\ = "\n";
-
 use strict;
 use warnings FATAL => 'all';
 use diagnostics;
+use Carp;
+use English qw( -no_match_vars);
 
-print "DIGITE O NOME DO FUCKING ARQUIVO\n";
-chomp (my $file=<STDIN>);
+local $INPUT_RECORD_SEPARATOR = "\n";
+
+
+print "DIGITE O NOME DO FUCKING ARQUIVO\n" or croak 'bla';
+chomp (my $file=<ARGV>);
 
 my $hora = (-M $file)*24;
-print $hora;
+print $hora or croak 'bla2';
 my $minuto = $hora*60;
-printf "%.2f",$minuto;
+printf '%.2f',$minuto;
 
